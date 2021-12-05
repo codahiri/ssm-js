54009639ad571e242371f2fbb5da5bc8dfa285fd
ilosrim <ilosrim@yahoo.com>
2021-12-05 22:24:11 +0500

    supermathjs

---
+++ index.js
@@ -0,0 +1,24 @@
+// adds the given numbers
+module.exports.plus = function plus(a, b) {
+  return a + b;
+}
+
+// subtracts the given numbers
+module.exports.minus = function minus(a, b) {
+  return a - b;
+}
+
+// multiplies the given numbers
+module.exports.multiply = function multiply(a, b) {
+  return a * b;
+}
+
+// divide the given numbers
+module.exports.divide = function divide(a, b) {
+  return a / b;
+}
+
+// returns the level of a given number
+module.exports.degree = function degree(a) {
+  return a ** 2
+}
